d8bdfea278a2ded406f0741adc6189e390a7fb41
RikvanderSchalie <79224832+RikvanderSchalie@users.noreply.github.com>
2021-05-18 22:11:31 +0200

    createButtons

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import './App.css';
 
 function App() {
   return (
+     <nav>
+
       <h1>Handbags & Purses</h1>
+
+       <button
+           onClick={() => console.log( "to the collection")}>
+           to the collection
+       </button>
+
+         <button
+             onClick={() => console.log( "shop al bags")}>
+         > shop all bags
+         </button>
+
+         <button
+             //onClick={() => console.log( "pre-orders")}
+         > pre-orders </button>
+
+     </nav>
   );
 }
 
