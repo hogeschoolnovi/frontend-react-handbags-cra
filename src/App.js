d90c921279b9c60ba04f4df90ee4ad8b0e2340c4
RikvanderSchalie <79224832+RikvanderSchalie@users.noreply.github.com>
2021-05-20 11:04:08 +0200

    add ALL products as cmpnt

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
-import foto from './bag_1.png';
+import bag1 from './bag_1.png';
+import bag2 from './bag_2.png';
+import bag3 from './bag_3.png';
+import bag4 from './bag_4.png';
 import Button from "./Button";
 import Product from "./Product";
 
@@ -10,7 +13,7 @@ import Product from "./Product";
 
 function App() {
   return (
-      <main>
+
      <nav>
 
       <h1>Handbags & Purses</h1>
@@ -19,20 +22,40 @@ function App() {
          <Button subject ={"Shop All Bags"}/>
          <Button subject ={"Pre-Orders"}/>
 
-     </nav>
 
+    <main>
 
-          <article>
-              <span>Best seller</span>
-              <img src={foto} alt={"tas"}/>
-              <p> The handy bag  </p>
-              <h4>350,-</h4>
-          </article>
+          <Product
+              span="Best-seller"
+              image={bag1}
+              omschrijving="The handy bag"
+              prijs="€350,-"
+          />
 
-          <Product/>
-          <Product/>
-          <Product/>
+        <Product
+            span="Best-seller"
+            image={bag2}
+            omschrijving="The stylish bag"
+            prijs="€250,-"
+        />
+
+        <Product
+            span="New collection"
+            image={bag2}
+            omschrijving="The simple bag"
+            prijs="€300,-"
+        />
+
+        <Product
+            span="New collection"
+            image={bag2}
+            omschrijving="The trendy bag"
+            prijs="€150,-"
+        />
+
 </main>
+
+</nav>
   );
 }
 
--- src/Product.js
+++ src/Product.js
@@ -1,13 +1,35 @@
-import foto from "./bag_1.png";
+
 import React from "react";
 
-function Product(){
+
+/*function Product(){
 return (
     <article className={'product'}>
         <span>Best seller</span>
         <img src={foto} alt={"tas"}/>
         <p className={'product'}> The handy bag  </p>
         <h4 className={'product'}>350,-</h4>
+    </article>
+)
+}*/
+
+/*<Product
+    span="Best-seller"
+    image={bag1}
+    omschrijving="The handy bag"
+    prijs="350,-"
+
+/>*/
+
+function Product({span, image, omschrijving, prijs}){
+
+return (
+    <article className="product">
+        <span className="product-span">{span}</span>
+        <img src={image} alt={omschrijving}/>
+        <p className="product-naam">{omschrijving}</p>
+        <h4 className="product-prijs">{prijs}</h4>
+
     </article>
 )
 }
