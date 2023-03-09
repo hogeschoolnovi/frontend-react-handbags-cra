6518a2e98febac7f667faf04d7b2e4f656269bb3
Rodney van den Velden <rodneymichael2002@gmail.com>
2023-03-09 16:52:40 +0100

    Forgot console.log in buttons

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,5 @@
 export default function Button({ children, disabled }: { children: string, disabled: boolean }) {
   return (
-    <button disabled={ disabled }>{ children }</button>
+    <button disabled={ disabled } onClick={() => console.log({ children })}>{ children }</button>
   );
 }
